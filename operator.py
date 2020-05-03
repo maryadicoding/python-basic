79c8d67a3702b32aef95c5c6ca9778195793e349
maryadi <maryadi071@gmail.com>
2020-05-03 22:40:01 +0700

    add operator

---
+++ operator.py
@@ -0,0 +1,55 @@
+a=7
+b=5
+
+"""
+Operator
++ = Add two operands or unary plus ex. x + y+ 2
+- = Subtract right operand from the left or unary minus ex. x - y- 2
+* = Multiply two operands ex. x * y
+/ = Divide left operand  right one (always results into float) ex. x / y
+% = Modulus - remainder of the division of left operand by the right ex. x % y (remainder of x/y) 
+// = Floor division - division that results into whole number adjusted to the left in the number line ex. x // y
+** = Exponent - left operand raised to the power of right x**y (x to the power y)
+"""
+print("operator")
+
+print("a = 7, b = 5")
+#penjumlahan
+print("a + b =", a+b)
+
+#perkalian
+print("a x b =", a*b)
+
+#pembagian
+print("a : b =", a/b)
+
+#pengurangan
+print("a - b =", a-b)
+
+#floor division
+print("a // b =", a//b)
+
+#eksponen
+print("a ** b =", a**b)
+
+#modulus
+print("a % b =", a%b)
+
+"""
+> = Greater than - True if left operand is greater than the right ex. x > y
+< = Less than - True if left operand is less than the right ex. x < y
+== = Equal to - True if both operands are equal ex. x == y
+!= = Not equal to - True if operands are not equal ex. x != y
+>= = Greater than or equal to - True if left operand is greater than or equal to the right ex. x >= y
+<= = Less than or equal to - True if left operand is less than or equal to the right ex. x <= y
+"""
+print("Operator pembanding")
+
+print("a = 10, b = 12")
+
+print("a > b adalah ", a > b)
+print("a < b adalah ", a < b)
+print("a = b adalah ", a == b)
+print("a != b adalah ", a != b)
+print("a >= b adalah ", a >= b)
+print("a <= b adalah ", a <= b)
---
+++ readme.md
@@ -0,0 +1,3 @@
+## references
+
+- https://www.programiz.com
